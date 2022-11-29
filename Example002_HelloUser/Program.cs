285e250399f8594462993491dbce1dda9b8412a6
Yura Karbushev <yurakarbushev@gmail com>
2022-11-29 21:59:04 +0700

    Add Example003_Sum

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите свое имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
